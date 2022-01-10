0242147cc8841b61a3d6afa0cdda52d16eec0920
Mathea Hansen <matheagth@live.dk>
2022-01-10 18:10:03 +0100

    updates til beskrivelser bl.a.

---
+++ script.js
@@ -0,0 +1,41 @@
+/*window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  console.log("toggleMenu");
+  document.querySelector("#topnav1").classList.toggle("hidden");
+
+  let erSkjult = document
+    .querySelector("#topnav1")
+    .classList.contains("hidden");
+
+  if (erSkjult == true) {
+    document.querySelector("#menuknap").textContent = "☰";
+  } else {
+    document.querySelector("#menuknap").textContent = "X";
+  }
+}*/
+const btn = document.querySelector("button");
+const ul = document.querySelector("nav");
+
+function toggleMenu() {
+  ul.classList.toggle("shown");
+
+  const menu = ul.classList.contains("shown");
+
+  if (menu) {
+    // hvis ul har klassen "shown"
+    //btn.textContent = "Luk";
+    btn.classList.add("open");
+  } else {
+    // hvis IKKE ul har klassen "shown"
+    //btn.textContent = "Menu";
+    btn.classList.remove("open");
+  }
+}
+
+btn.addEventListener("click", toggleMenu);
